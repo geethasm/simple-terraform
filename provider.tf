0792ee1fcf277ea8c81c52f1bbe8afbe246e8ce4
Geetha Sathyamurthy <geetha_sathyamurthy@in.ibm.com>
2023-11-02 17:22:01 +0530

    added provider details in provider.tf

--- provider.tf
+++ provider.tf
@@ -3,3 +3,15 @@
   generation = 2
   region = "us-south"
 }*/
+//required_version = "~> 1.1"
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "1.43.0"
+    }
+  }
+}
+
+provider "ibm" {
+  # Configuration options
+}
